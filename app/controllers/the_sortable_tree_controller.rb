668ccbac99c3ea2addbbb2ad9e37641ce28a138e
Idris <sld7700@gmail.com>
2012-08-29 16:50:00 +0600

    Added EngineRebuild to work in engines 'cause engine models looks like EngineModule::SomeModel

--- app/controllers/the_sortable_tree_controller.rb
+++ app/controllers/the_sortable_tree_controller.rb
@@ -6,8 +6,16 @@ module TheSortableTreeController
     public
     def the_define_common_variables
       collection = self.class.to_s.split(':').last.sub(/Controller/,'').underscore.downcase                 # recipes
-      variable   = collection.singularize                                                                   # recipe  
+      variable   = collection.singularize                                                                   # recipe
       klass      = self.respond_to?(:sortable_model) ? self.sortable_model : variable.classify.constantize  # Recipe
+      ["@#{variable}", collection, klass]
+    end
+
+    def the_define_common_variables_for_engine
+      collection = self.class.to_s.split(':').last.sub(/Controller/,'').underscore.downcase                 # recipes
+      variable   = collection.singularize                                                                   # recipe
+      modules_of_klass = self.class.to_s.split('::')[0...-1]
+      klass      = self.respond_to?(:sortable_model) ? self.sortable_model : ( (modules_of_klass.join("/") + "/" + variable ).classify.constantize )  # Recipe or SomeModule::Recipe
       ["@#{variable}", collection, klass]
     end
   end
@@ -63,4 +71,32 @@ module TheSortableTreeController
       render(:nothing => true)
     end
   end
+  
+  # For including on Engine models, 'cause engine models looks like EngineFoo::Model
+  module EngineRebuild
+    include DefineVariablesMethod
+    public
+    def rebuild
+      id        = params[:id].to_i
+      parent_id = params[:parent_id].to_i
+      prev_id   = params[:prev_id].to_i
+      next_id   = params[:next_id].to_i
+
+      render :text => "Do nothing" and return if parent_id.zero? && prev_id.zero? && next_id.zero?
+
+      variable, collection, klass = self.the_define_common_variables_for_engine
+      variable = self.instance_variable_set(variable, klass.find(id))
+
+      if prev_id.zero? && next_id.zero?
+        variable.move_to_child_of klass.find(parent_id)
+      elsif !prev_id.zero?
+        variable.move_to_left_of klass.find(prev_id)
+      elsif !next_id.zero?
+        variable.move_to_right_of klass.find(next_id)
+      end
+
+      render(:nothing => true)
+    end
+  end
+
 end
