07770a2520a4c117d9f42793c3afceee7521bf13
Ilya N. Zykin <zykin-ilya@ya.ru>
2012-11-20 15:22:55 +0400

    ~~~

--- app/helpers/tree_render_helper.rb
+++ app/helpers/tree_render_helper.rb
@@ -11,4 +11,16 @@ module TreeRenderHelper
       </li>
     "
   end
+
+  def show_link options = {}
+    node = options[:node]
+    ns   = options[:opts][:namespace]
+    "<h4>#{ link_to(node.title, node) } | #{ url_for(ns + [node]) }</h4>"
+  end
+
+  def children options = {}
+    unless options[:children].empty?
+      "<ol class='nested_set'>#{options[:children]}</ol>"
+    end
+  end
 end
