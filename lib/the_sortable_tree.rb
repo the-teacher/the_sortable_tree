d3f79d61576653fad037088f21c3b7f444084497
Ilya N. Zykin <zykin-ilya@ya.ru>
2013-01-19 13:36:28 +0400

    2.0.0 release

--- lib/the_sortable_tree.rb
+++ lib/the_sortable_tree.rb
@@ -6,8 +6,8 @@ module TheSortableTree
   module Scopes
     def self.included(base)
       base.class_eval do
-        scope :nested_set,          order('lft ASC')
-        scope :reversed_nested_set, order('lft DESC')
+        scope :nested_set,          -> { order('lft ASC')  }
+        scope :reversed_nested_set, -> { order('lft DESC') }
       end
     end
   end
